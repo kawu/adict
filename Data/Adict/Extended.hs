aae670664572de4b3092d4c6e98b9651bec7524f
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-05-17 13:12:20 +0200

    Add Entry data type

--- Data/Adict/Extended.hs
+++ Data/Adict/Extended.hs
@@ -1,7 +1,7 @@
 module Data.Adict.Extended
 ( search
 , Rule
-, Res
+, Entry (..)
 ) where
 
 import Data.List (isPrefixOf)
@@ -12,11 +12,20 @@ import Data.RadixTree
 -- | Substitution ([a] -> [a]) with given cost.
 type Rule a = ([a], Double, [a])
 
--- | Search result.
-type Res a b = ([a], b)
+-- | Dinctionary entry.
+data Entry a b = Entry
+    { word :: [a]
+    , info :: b }
+    deriving Show
 
-search :: (Ord a, Ord b) => [Rule a] -> Double -> [a]
-       -> Trie a b -> [(Res a b, Double)]
+instance Eq a => Eq (Entry a b) where
+    Entry x _ == Entry y _ = x == y
+
+instance Ord a => Ord (Entry a b) where
+    Entry x _ <= Entry y _ = x <= y
+
+search :: Ord a => [Rule a] -> Double -> [a]
+       -> Trie a b -> [(Entry a b, Double)]
 search rs th p trie
      = M.toList $ M.fromListWith min
      $ doSearch ([], 0.0) rs th p trie
@@ -24,10 +33,10 @@ search rs th p trie
 -- | Find all words in a trie with overall cost lower or equall
 -- to given threshold.
 doSearch :: Eq a => ([a], Double) -> [Rule a] -> Double -> [a]
-         -> Trie a b -> [(Res a b, Double)]
+         -> Trie a b -> [(Entry a b, Double)]
 doSearch (path, k) _ _ [] trie =
     case valueIn trie of
-        Just x  -> [((path, x), k)]
+        Just x  -> [(Entry path x, k)]
         Nothing -> []
 doSearch (path, k) rs th p trie = concat
     [ case follow x' trie of
