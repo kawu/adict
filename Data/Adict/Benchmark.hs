cf1f23acb2a1dcd6a97c29391a2d15454d7c59c8
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2012-07-03 18:54:32 +0200

    Adapt benchmarks to new hierarchy

--- Data/Adict/Benchmark.hs
+++ Data/Adict/Benchmark.hs
@@ -3,29 +3,29 @@ import Control.Applicative ((<$>))
 import Criterion.Main
 import Test.QuickCheck
 import Test.QuickCheck.Gen
-import qualified Data.Vector.Unboxed as U
 
-import qualified Data.Adict as A
+import qualified Data.Adict.Slow as S
 import qualified Data.Adict.Fast as F
 import qualified Data.Adict.Check as C
+import qualified Data.Trie as T
+import qualified Data.Trie.Class as T hiding (Trie)
+import Data.Adict.Base
 
-type Word = U.Vector Char
-
-benchmarkSearch :: A.Cost Char -> Double -> A.Trie Char ()
+benchmarkSearch :: Cost Char -> Double -> T.Trie (Maybe ())
                 -> Word -> Benchmark
 benchmarkSearch cost k trie x = bgroup ("Search-" ++ desc)
-    [ bench "Opt"  $ whnf (length . flip (F.levenSearch cost k) trie) x
-    , bench "Base" $ whnf (length . flip (A.levenSearch cost k) trie) x ]
+    [ bench "Opt"  $ whnf (length . flip (F.search cost k) trie) x
+    , bench "Base" $ whnf (length . flip (S.search cost k) trie) x ]
   where
-    desc = "(" ++ show (A.size trie) ++ ", " ++ show (U.length x) ++ ")"
+    desc = "(" ++ show (T.size trie) ++ ", " ++ show (wordSize x) ++ ")"
 
 string :: Gen String
 string = listOf $ elements $ ['a'..'z'] ++ ['A'..'Z']
 
-arbitraryPoint :: Int -> Int -> Gen (A.Trie Char (), Word)
+arbitraryPoint :: Int -> Int -> Gen (T.Trie (Maybe ()), Word)
 arbitraryPoint n k = do
-    trie <- A.fromLang <$> vector n
-    x    <- U.fromList <$> vector k
+    trie <- T.fromLang <$> vector n
+    x    <- fromString <$> vector k
     return (trie, x)
 
 get :: Gen a -> IO a
@@ -41,7 +41,7 @@ main = do
         [ get $ arbitraryPoint n k
         | n <- ns, k <- ks ]
 
-    let cost = A.costDefault
+    let cost = costDefault
     let th   = 1.0
     -- cost <- C.fromDesc <$> get arbitrary    -- ^ Arbitrary cost function
     -- th  <- getPositive <$> get arbitrary    -- ^ Threshold
